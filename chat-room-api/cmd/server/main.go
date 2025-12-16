3165f3cc8b8c376f311e0bd734aacf44f4815a54
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-12-16 16:43:28 -0300

    refactor: enhancing code quality in server's main.go

--- chat-room-api/cmd/server/main.go
+++ chat-room-api/cmd/server/main.go
@@ -5,24 +5,12 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 	"github.com/rafaeldepontes/go-chat/internal/middleware"
 	"github.com/rafaeldepontes/go-chat/internal/tool"
-	// "github.com/rafaeldepontes/go-chat/pkg/db/postgres"
+
 	"github.com/rafaeldepontes/go-chat/pkg/message-broker/rabbitmq"
 )
-
-const (
-	ReadBufferSize  = 4096
-	WriteBufferSize = 4096
-)
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  ReadBufferSize,
-	WriteBufferSize: WriteBufferSize,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
 
 func main() {
 	envFile := ".env"
@@ -36,26 +24,33 @@ func main() {
 	port := os.Getenv("SERVER_PORT")
 
 	var serverURL string
-	serverURL = os.Getenv("SERVER_URL")
-	if os.Getenv("IS_TLS") != "false" {
-		serverURL = os.Getenv("TLS_SERVER_URL")
-	}
+	getServerUrl(&serverURL)
 
-	handler := middleware.NewHandler(&upgrader)
+	handler := middleware.NewHandler()
 
 	go handler.Server.Run()
-	// defer postgres.Disconnect()
 	defer rabbitmq.CloseConn()
 	defer rabbitmq.CloseChan()
 
 	fmt.Printf("API running on %v port\n", serverURL)
-	if os.Getenv("IS_TLS") != "false" {
-		if err := http.ListenAndServeTLS(":"+port, os.Getenv("SERVER_CERTIFICATE"), os.Getenv("SERVER_KEY"), handler); err != nil {
-			fmt.Println("Error initializing the api:", err)
-		}
-	} else {
+	run(port, handler)
+}
+
+func run(port string, handler http.Handler) {
+	if os.Getenv("IS_TLS") == "false" {
 		if err := http.ListenAndServe(":"+port, handler); err != nil {
 			fmt.Println("Error initializing the api:", err)
 		}
+	} else {
+		if err := http.ListenAndServeTLS(":"+port, os.Getenv("SERVER_CERTIFICATE"), os.Getenv("SERVER_KEY"), handler); err != nil {
+			fmt.Println("Error initializing the api:", err)
+		}
+	}
+}
+
+func getServerUrl(serverURL *string) {
+	*serverURL = os.Getenv("SERVER_URL")
+	if os.Getenv("IS_TLS") != "false" {
+		*serverURL = os.Getenv("TLS_SERVER_URL")
 	}
 }
