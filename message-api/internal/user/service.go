3e18a867b5d700ac27a8e365d05f138a1988db5e
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-12-16 20:35:48 -0300

    feat: adding the gRPC logic to the service interface

--- message-api/internal/user/service.go
+++ message-api/internal/user/service.go
@@ -1,7 +1,14 @@
 package user
 
+import (
+	"context"
+
+	pb "github.com/rafaeldepontes/go-chat/shared/message"
+)
+
 type Service interface {
 	GetUserChannel() *chan []byte
-	FindAll() ([]byte, error)
+	FindAll(context.Context, *pb.MessageRequest) (*pb.MessageResponses, error)
 	Save()
+	Run() error
 }
