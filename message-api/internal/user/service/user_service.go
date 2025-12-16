9f2b4f931195560e9f26ded2cacf0d3ecc343d5c
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-12-16 20:36:26 -0300

    feat: using gRPC in the UserService

--- message-api/internal/user/service/user_service.go
+++ message-api/internal/user/service/user_service.go
@@ -1,37 +1,56 @@
 package service
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"net"
+	"os"
 
 	"github.com/rafaeldepontes/go-chat/internal/model"
 	"github.com/rafaeldepontes/go-chat/internal/user"
 	"github.com/rafaeldepontes/go-chat/internal/user/repository"
+	pb "github.com/rafaeldepontes/go-chat/shared/message"
+	"google.golang.org/grpc"
 )
 
 type userSvc struct {
+	pb.UnimplementedMessageServiceServer
+	grpcServer *grpc.Server
 	repository user.Repository
 	msg        chan []byte
 }
 
 func NewService() user.Service {
-	return &userSvc{
+	grpcServer := grpc.NewServer()
+
+	userSvc := &userSvc{
+		grpcServer: grpcServer,
 		repository: repository.NewRepository(),
 		msg:        make(chan []byte),
 	}
+
+	pb.RegisterMessageServiceServer(grpcServer, userSvc)
+
+	return userSvc
 }
 
 func (s *userSvc) GetUserChannel() *chan []byte {
 	return &s.msg
 }
 
-func (s *userSvc) FindAll() ([]byte, error) {
+func (s *userSvc) FindAll(context.Context, *pb.MessageRequest) (*pb.MessageResponses, error) {
 	chats, err := s.repository.FindAll()
+
 	if err != nil {
 		return nil, err
 	}
 
-	return json.Marshal(chats)
+	resp := &pb.MessageResponses{
+		Data: chats,
+	}
+
+	return resp, nil
 }
 
 func (s *userSvc) Save() {
@@ -42,4 +61,16 @@ func (s *userSvc) Save() {
 		}
 		s.repository.Save(&user)
 	}
+}
+
+func (s *userSvc) Run() error {
+	var port string
+	port = ":" + os.Getenv("SERVER_PORT")
+
+	list, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println("[Error] connection to: ", os.Getenv("SERVER_PORT"))
+	}
+
+	return s.grpcServer.Serve(list)
 }
